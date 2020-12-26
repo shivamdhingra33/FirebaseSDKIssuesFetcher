50f720ac9c63d6d7651f1febac781ac42fa7bffd
Shivam Dhingra <shivam.dhingra@fleetx.io>
2020-12-26 18:46:30 +0530

    Initial Commit

---
+++ FirebaseSDKIssuesFetcher/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseSDKIssuesFetcher
+//
+//  Created by Shivam Dhingra on 26/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
